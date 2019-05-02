0d788f50a6c5b2b274efbd7c332718213bb3a5e4
Jen <jenniferanndcb@gmail.com>
2019-05-02 10:08:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2
     array.join (" and ")
     else array.length >= 3 
-    array[-1] = "and #{array.last}"
-    array.join (",") 
+    array[-1] = " and #{array.last}"
+    array.join (" , ") 
   end
 end
