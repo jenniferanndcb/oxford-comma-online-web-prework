8ce455eafff64923897e830c71a701d5e6fb2b7f
Jen <jenniferanndcb@gmail.com>
2019-05-02 10:06:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2
     array.join (" and ")
     else array.length >= 3 
-    array[-1] << "and"
+    array[-2] << " and "
     array.join (",") 
   end
 end
